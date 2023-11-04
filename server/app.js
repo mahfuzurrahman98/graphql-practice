b63ee00d751c7dca7d00610ba9e1dab2e95e8453
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2023-11-04 21:29:46 +0600

    using static database insted of third party

--- server/app.js
+++ server/app.js
@@ -3,8 +3,11 @@ const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { default: axios } = require('axios');
 const PORT = 4000;
+
+const database = require('./fakeDatabase');
+
+// console.log(database.users[0].name);
 
 async function startServer() {
   const app = express();
@@ -36,50 +39,23 @@ async function startServer() {
     resolvers: {
       Todo: {
         user: async (parent) => {
-          const user = await axios
-            .get(`https://jsonplaceholder.typicode.com/users/${parent.userId}`)
-            .then((res) => res.data)
-            .catch((err) => console.log(err));
+          const user = database.users.find((user) => user.id === parent.userId);
           return user;
         },
       },
       User: {
         todos: async (parent) => {
-          const todos = await axios
-            .get(
-              `https://jsonplaceholder.typicode.com/todos?userId=${parent.id}`
-            )
-            .then((res) => res.data)
-            .catch((err) => console.log(err));
+          const todos = database.todos.filter(
+            (todo) => todo.userId === parent.id
+          );
           return todos;
         },
       },
       Query: {
-        getTodos: () =>
-          axios
-            .get('https://jsonplaceholder.typicode.com/todos')
-            .then((res) => res.data)
-            .catch((err) => console.log(err)),
-        getUsers: () =>
-          axios
-            .get('https://jsonplaceholder.typicode.com/users')
-            .then((res) => res.data)
-            .catch((err) => console.log(err)),
-        getUser: (_, { id }) =>
-          axios
-            .get(`https://jsonplaceholder.typicode.com/users/${id}`)
-            .then((res) => res.data)
-            .catch((err) => console.log(err)),
-        getTodo: (_, { id }) =>
-          axios
-            .get(`https://jsonplaceholder.typicode.com/todos/${id}`)
-            .then((res) => res.data)
-            .catch((err) => console.log(err)),
-        getUserTodos: (_, { id }) =>
-          axios
-            .get(`https://jsonplaceholder.typicode.com/todos?userId=${id}`)
-            .then((res) => res.data)
-            .catch((err) => console.log(err)),
+        getTodos: () => database.todos,
+        getUsers: () => database.users,
+        getUser: (_, { id }) => database.users.find((user) => user.id === id),
+        getTodo: (_, { id }) => database.todos.find((todo) => todo.id === id),
       },
     },
   });
